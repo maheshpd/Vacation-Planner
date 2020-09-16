5c3d482864f1fd75f5bb2921c809d12cd9ad4efb
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-09-16 21:18:10 +0530

    Vacation Planner - Part 2

--- VacationPlanner/Views/HomeView.swift
+++ VacationPlanner/Views/HomeView.swift
@@ -8,28 +8,45 @@
 
 import SwiftUI
 
+struct Continents: Identifiable {
+    var id = UUID()
+    var name: String
+    var color: Color
+}
+
+
 struct HomeView: View {
     
     @State var val:String = ""
+    var listOfCountients:[Continents] = [
+        Continents.init(name: "Europe", color: Color.red),
+        Continents.init(name: "Asia", color: Color.blue),
+        Continents.init(name: "Africa", color: Color.green),
+        Continents.init(name: "South America", color: Color.yellow),
+        Continents.init(name: "North America", color: Color.orange)
+    ]
     
     var body: some View {
         VStack(spacing:0){
             TextField("Placeholder", text: $val)
                 .padding()
                 .background(Color.red)
+            
+            
             ScrollView(.horizontal,showsIndicators: false){
                 HStack{
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                    Text("text")
-                }.padding()
+                    Spacer().frame(width: 10)
+                    ForEach(listOfCountients,id: \.id){thisContinent in
+                        
+                        Text("\(thisContinent.name)").padding()
+                            .background(thisContinent.color)
+                            .cornerRadius(10)
+                            .font(.system(size: 15, weight: .bold))
+                    }
+                    
+                    
+                    
+                }.padding([.top,.bottom], 20)
                     .background(Color.green)
             }
             
